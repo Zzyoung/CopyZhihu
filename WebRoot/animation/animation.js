02a490a233444425c04ddcb5f4e737d4eb07310e
ZhangYang <skybabby@126.com>
2015-11-09 22:53:07 +0800

    使用面向对象、观察者模式重写“回到顶部”按钮的功能及行为

--- WebRoot/animation/animation.js
+++ WebRoot/animation/animation.js
@@ -1,3 +1,47 @@
+(function(){
+  var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
+ 
+  this.Class = function(){};
+ 
+  Class.extend = function(prop) {
+    var _super = this.prototype;
+   
+    initializing = true;
+    var prototype = new this();
+    initializing = false;
+   
+    for (var name in prop) {
+      prototype[name] = typeof prop[name] == "function" &&
+        typeof _super[name] == "function" && fnTest.test(prop[name]) ?
+        (function(name, fn){
+          return function() {
+            var tmp = this._super;
+           
+            this._super = _super[name];
+           
+            var ret = fn.apply(this, arguments);        
+            this._super = tmp;
+           
+            return ret;
+          };
+        })(name, prop[name]) :
+        prop[name];
+    }
+   
+    function Class() {
+      if ( !initializing && this.init )
+        this.init.apply(this, arguments);
+    }
+   
+    Class.prototype = prototype;
+   
+    Class.prototype.constructor = Class;
+ 
+    Class.extend = arguments.callee;
+   
+    return Class;
+  };
+})();
 window.onload = function(){
 	var timeout = null;
 	var msgBox = document.getElementById("zh-top-nav-count-wrap");
@@ -163,32 +207,119 @@ window.onload = function(){
 		})(i);
 	};
 	
-	var backtotop = document.querySelector('.zh-backtotop');
-
-	window.onscroll = function(){
-		if(Utils.getBodyScrollTop()>1200){
-			backtotop.style.display = "block";
-			backtotop.onclick = function(){
-				var body = null;
-				if(document.body.scrollTop){
-					body = document.body;
-				}else{
-					body = document.documentElement;
-				}
-				setTimeout(function(){
-					if (body.scrollTop<200) {
-						body.scrollTop =0;
-					}else{
-						body.scrollTop -=200;
-					}
-					if (body.scrollTop>0) {
-						setTimeout(arguments.callee,30);
-					};
-				},30);
-			};
-		}else{
-			backtotop.style.display = "none";
-			backtotop.onclick = null;
-		}
-	};
+	var doc = new Doc();
+	var backToTop = new BackToTop({
+		button:document.querySelector('.zh-backtotop'),
+		isShow:false
+	});
+	BackToTop.prototype.on('click','scrollToTop',doc);
+	Doc.prototype.on('scroll','toggle',backToTop);
 };
+var Base = Class.extend({
+	init : function(config){
+		this._config = config;
+		this.bind();
+		this.render();
+	},
+	get : function(key){
+		return this._config[key];
+	},
+	set : function(key,value){
+		this._config[key] = value;
+	},
+	bind : function(){
+		//绑定事件
+	},
+	render : function(){
+		//渲染
+	},
+	destory : function(){
+		//销毁
+	},
+	//观察者模式
+	on : function(type, fn, context){
+		type = type || 'any';
+		fn = typeof fn === 'function' ? fn : context[fn];
+		
+		if(typeof this.subscribers[type] === 'undefined'){
+			this.subscribers[type] = [];
+		} 
+		this.subscribers[type].push({fn : fn, context : context || this});
+	},
+	off : function(type, fn, context){
+		this.visitSubscribers('unsubscribe', type, fn, context);
+	},
+	fire : function(type, publication) {
+		this.visitSubscribers('publish', type, publication);
+	},
+	visitSubscribers: function(action, type, arg, context){
+		var pubtype = type || 'any',
+			subscribers = this.subscribers[pubtype],
+			i,
+			count = subscribers ? subscribers.length : 0;
+		for(i = 0; i < count; i += 1){
+			if(action === 'publish'){
+				subscribers[i].fn.call(subscribers[i].context, arg);
+			} else {
+				if(subscribers[i].fn === arg && subscribers[i].context === context){
+					subscribers.splice(i, 1);
+				}
+			}
+		}
+	}
+});
+var Doc = Base.extend({
+	subscribers:{
+		any:[]
+	},
+	bind:function(){
+		var me = this;
+		window.onscroll = function(){
+			me.fire('scroll');
+		};
+	},
+	scrollToTop:function(){
+		var body = null;
+		body = document.body.scrollTop ? body = document.body : document.documentElement;
+		setTimeout(function(){
+			if (body.scrollTop < 200) {
+				body.scrollTop = 0;
+			}else{
+				body.scrollTop -= 200;
+			}
+			if (body.scrollTop>0) {
+				setTimeout(arguments.callee,30);
+			};
+		},30);
+	}
+});
+var BackToTop = Base.extend({
+	subscribers:{
+		any:[]
+	},
+	bind:function(){
+		var me = this;
+		this.get('button').onclick = function(){
+			me.fire('click');
+		};
+	},
+	toggle:function(){
+		if(Utils.getBodyScrollTop()>1200){
+			this.show();
+		}else{
+			this.hide();
+		}
+	},
+	hide:function(){
+		if(this.get('isShow')){
+			this.get('button').style.display = "none";
+			this.set('isShow',false);
+		}
+	},
+	show:function(){
+		if(!this.get('isShow')){
+			this.get('button').style.display = "block";
+			this.set('isShow',true);
+		}
+	}
+});
